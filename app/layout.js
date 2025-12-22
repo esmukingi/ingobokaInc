423fb3b9f88fef62940755e6c28084124f50f378
infinexa <infinexa@gmail.com>
2025-12-23 00:54:28 +0200

    Fixing Server-Client Issue

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,12 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "react-hot-toast";
-import ClientLayout from "@/layout/clientLayout";
+import ClientLayoutWrapper from "@/components/ClientLayoutWrapper";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
   variable: "--font-poppins",
-  display: "swap"
+  display: "swap",
 });
 
 export const metadata = {
@@ -15,7 +14,7 @@ export const metadata = {
 
   title: {
     default: "Sisitemu yo gucunga farumasi | Ingoboka",
-    template: "%s | IngobokaInc"
+    template: "%s | IngobokaInc",
   },
 
   description:
@@ -33,7 +32,7 @@ export const metadata = {
     "Ingoboka Inc",
     "Ingoboka pharmacy system",
     "Ingoboka farumasi",
-    "sisitemu ya farumasi"
+    "sisitemu ya farumasi",
   ],
 
   applicationName: "IngobokaInc",
@@ -53,9 +52,9 @@ export const metadata = {
         url: "/og-image.png",
         width: 1200,
         height: 630,
-        alt: "Ingoboka Pharmacy Management Software"
-      }
-    ]
+        alt: "Ingoboka Pharmacy Management Software",
+      },
+    ],
   },
 
   twitter: {
@@ -63,7 +62,7 @@ export const metadata = {
     title: "Ingoboka – Sisitemu ya Farumasi",
     description:
       "Sisitemu ifasha farumasi gucunga imiti n'igihe izarangirira mu buryo bworoshye.",
-    images: ["/og-image.png"]
+    images: ["/og-image.png"],
   },
 
   manifest: "/manifest.json",
@@ -71,38 +70,38 @@ export const metadata = {
   appleWebApp: {
     capable: true,
     title: "Ingoboka",
-    statusBarStyle: "black-translucent"
+    statusBarStyle: "black-translucent",
   },
 
   icons: {
     icon: "/favicon.ico",
-    apple: "/apple-icon.png"
+    apple: "/apple-icon.png",
   },
 
   robots: {
     index: true,
-    follow: true
+    follow: true,
   },
+
   other: [
     {
       name: "google-site-verification",
-      content: "0R26gDoOtNCnmjLMaIZ87eu1VIZiw1C-g7GBG_L7zxU"
-    }
-  ]
+      content: "0R26gDoOtNCnmjLMaIZ87eu1VIZiw1C-g7GBG_L7zxU",
+    },
+  ],
 };
- 
+
 export const viewport = {
   themeColor: "#1e7f5c",
   width: "device-width",
-  initialScale: 1
+  initialScale: 1,
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="rw" className={poppins.variable}>
       <body className="antialiased bg-white text-gray-900">
-        <ClientLayout>{children}</ClientLayout>
-        <Toaster position="bottom-right" />
+        <ClientLayoutWrapper>{children}</ClientLayoutWrapper>
       </body>
     </html>
   );
---
+++ components/ClientLayoutWrapper.js
@@ -0,0 +1,13 @@
+"use client";
+
+import { Toaster } from "react-hot-toast";
+import ClientLayout from "@/layout/clientLayout";
+
+export default function ClientLayoutWrapper({ children }) {
+  return (
+    <ClientLayout>
+      {children}
+      <Toaster position="bottom-right" />
+    </ClientLayout>
+  );
+}
