8c1d7d764b5115beabb4b9014ed5c76994bfb471
esmukingi <125150567+esmukingi@users.noreply.github.com>
2025-12-23 01:48:29 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,15 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-});
+import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: [],
-  },
+  turbopack: {},
 };
 
-module.exports = withPWA(nextConfig);
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig);
+
